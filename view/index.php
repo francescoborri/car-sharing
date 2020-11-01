92d1069d3b2ffde43422e13073dbd5802790885e
francescoborri <francesco.borri02@gmail.com>
2020-11-01 18:18:29 +0100

    Create available car page

---
+++ view/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" href="../css/default.css">
+	<title>Auto disponibili</title>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row justify-content-center mb-md-4">
+			<div class="jumbotron jumbotron-fluid w-100">
+				<div class="row justify-content-around">
+					<div class="col-auto d-flex align-items-center text-center">
+						<h1 class="display-4">Auto disponibili</h1>
+					</div>
+					<div class="col-auto">
+						<form action="./" method="GET">
+							<ul class="list-group shadow">
+								<li class="list-group-item">
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text">Inizio</span>
+										</div>
+										<input type="date" class="form-control" name="start">
+									</div>
+								</li>
+								<li class="list-group-item">
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text">Fine</span>
+										</div>
+										<input type="date" class="form-control" name="end">
+									</div>
+								</li>
+							</ul>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
